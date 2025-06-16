b43c207af2e8bb42e36e57d93b6dbd99072ff1e2
Thyago-Monnerat <thy_thyago2.5@hotmail.com>
2025-06-16 13:45:36 -0300

    initial commit

---
+++ src/test/java/com/authMS/Auth/microsservice/AuthMicrosserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.authMS.Auth.microsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthMicrosserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
