a4c7c7a081d13257a1876280b34e9ab3a9c5a9ec
Thyago-Monnerat <thy_thyago2.5@hotmail.com>
2025-06-19 20:44:03 -0300

    fix: Roles changed to use only uppercase string

--- src/main/java/com/authMS/Auth/microsservice/enums/Role.java
+++ src/main/java/com/authMS/Auth/microsservice/enums/Role.java
@@ -4,12 +4,6 @@ import lombok.Getter;
 
 @Getter
 public enum Role {
-    ADMIN("admin"),
-    USER("user");
-
-    private final String role;
-
-    Role(String role) {
-        this.role = role;
-    }
+    ADMIN,
+    USER
 }
