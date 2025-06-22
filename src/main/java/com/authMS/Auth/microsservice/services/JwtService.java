1d22c5ba3179e9762567ef8d77a930007eee5f6d
Thyago-Monnerat <thy_thyago2.5@hotmail.com>
2025-06-22 20:27:59 -0300

    chore: JwtService no longer verifies token

--- src/main/java/com/authMS/Auth/microsservice/services/JwtService.java
+++ src/main/java/com/authMS/Auth/microsservice/services/JwtService.java
@@ -3,7 +3,6 @@ package com.authMS.Auth.microsservice.services;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
-import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.authMS.Auth.microsservice.dtos.UserDto;
 import com.authMS.Auth.microsservice.exceptions.JwtException;
 import org.springframework.beans.factory.annotation.Value;
@@ -29,32 +28,15 @@ public class JwtService {
             return JWT.create()
                     .withIssuer(issuer)
                     .withSubject(userDto.email())
-                    .withClaim("username", userDto.username())
-                    .withExpiresAt(generateExpirationDate(120))
+                    .withClaim("role", userDto.role().toString())
+                    .withExpiresAt(generateExpirationDate())
                     .sign(algorithm);
         } catch (JWTCreationException e) {
             throw new JwtException("Jwt creation exception " + e.getMessage());
         }
     }
 
-    public String validateToken(String token) {
-        Algorithm algorithm = Algorithm.HMAC256(key);
-        try {
-            return JWT.require(algorithm)
-                    .withIssuer(issuer)
-                    .build()
-                    .verify(token)
-                    .getSubject();
-        } catch (JWTVerificationException e) {
-            throw new JwtException("Jwt validation exception " + e.getMessage());
-        }
-    }
-
-    public String getTokenJti(String token){
-        return JWT.decode(token).getId();
-    }
-
-    private Instant generateExpirationDate(Integer minutes) {
-        return LocalDateTime.now().plusMinutes(minutes).toInstant(ZoneOffset.of("-03:00"));
+    private Instant generateExpirationDate() {
+        return LocalDateTime.now().plusMinutes(120).toInstant(ZoneOffset.of("-03:00"));
     }
 }
